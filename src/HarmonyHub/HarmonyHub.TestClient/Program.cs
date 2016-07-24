89f4a9ab045303216995d60bf306cc12bb763596
Michael Hallock <mike@hallock.net>
2016-07-24 05:03:12 -0400

    Added missing program file.

---
+++ src/HarmonyHub/HarmonyHub.TestClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace HarmonyHub.TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string username = "REDACTED ";
+            const string password = "REDACTED";
+            const string ip = "REDACTED";
+
+            using (var client = new Client(ip, username, password))
+            {
+                client.RequestConfig();
+                client.RequestCurrentActivity();
+
+                Thread.Sleep(2000);
+
+                var pvr = client.Config.Device.FirstOrDefault(x => x.Label == "Pace DVR");
+                var channelAction = pvr.ControlGroup.FirstOrDefault(x => x.Name == "Channel");
+                client.SendCommand(channelAction.Function.FirstOrDefault().Action);
+                Console.ReadLine();
+            }
+        }
+    }
+}
